13b83f08e8d5642dfb13944f7b4761cf6c5fdbeb
Adam Young <ayoung@sgi.sk.ca>
2026-08-02 11:44:26 -0600

    fix(mtp): compile worker slot scheduler

--- src/Stryker.Core/Stryker.Core/MutationTest/MutationTestProcess.cs
+++ src/Stryker.Core/Stryker.Core/MutationTest/MutationTestProcess.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO.Abstractions;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Stryker.Abstractions;
